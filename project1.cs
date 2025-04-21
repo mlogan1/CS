2e5a31cc7a160f1cc22182c4faad713aebd92ea9
Mel Logan Jr. <64439257+mlogan1@users.noreply.github.com>
2025-04-20 19:48:03 -0500

    Update project1.cs

--- project1.cs
+++ project1.cs
@@ -1,3 +1,7 @@
+/* Galactic Travel Agency
+You are tasked with managing passenger profiles for a Galactic Travel Agency. Each passenger has a name, age, ticket type, and preferred planet to visit. Using C#, you’ll start by creating these variables, manipulating them for different operations, and applying data conversions where needed.
+*/
+
 using System;
 
 public class GalacticTravelAgency { 
